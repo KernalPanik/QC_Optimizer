93b0700a0e6b03106fd809499f3b11360063ca4b
KernalPanik <lukas.michnevic@protonmail.com>
2021-03-07 17:55:04 +0200

    Added docs

--- Adaptive_Scheduler.py
+++ Adaptive_Scheduler.py
@@ -1,15 +1,20 @@
 from qiskit.transpiler import PassManager
-from qiskit import QuantumCircuit
 from Learner import OptimizationLearner
 
 class AdaptiveScheduler():
+    """
+    A Main entry point class.
+    Instantiate it and use it's methods to pass custom optimization schedules and
+    run Pass Manager passes.
+    """
     def __init__(self):
-        self.PassManager = PassManager()
-        self.Learner = OptimizationLearner()
-        
+        self.pass_manager = PassManager()
+        self.learner = OptimizationLearner()
+
     def run_optimization(self, quantum_circuit):
-        print("optimizing...")
-        return self.PassManager.run(quantum_circuit)
+        """Runs Qiskit Pass Manager"""
+        return self.pass_manager.run(quantum_circuit)
 
-
-
+    def add_optimization(self, optimization):
+        """Add Qiskit optimization pass to the Pass Manager"""
+        self.pass_manager.append(optimization)
--- test_optimization_cell.py
+++ test_optimization_cell.py
@@ -1,13 +1,6 @@
 #%%
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-from qiskit.dagcircuit import DAGCircuit
-from qiskit.converters import circuit_to_dag
-from qiskit.converters import dag_to_circuit
 from qiskit.transpiler.passes import CommutativeCancellation
-from qiskit.tools.visualization import dag_drawer
-from qiskit.transpiler import PassManager
-from qiskit.compiler import transpile
-from qiskit.test.mock import FakeMelbourne
 from Adaptive_Scheduler import AdaptiveScheduler
 
 q = QuantumRegister(3, 'q')
@@ -28,9 +21,6 @@ commutative_cancellation = CommutativeCancellation()
 adaptive_scheduler.PassManager.append(commutative_cancellation)
 transformed_circuit = adaptive_scheduler.run_optimization(circuit)
 
-#print(commutative_cancellation.name())
-#commutative_cancellation.run(dag)
-#circuit = dag_to_circuit(dag)
 print(circuit)
 print(transformed_circuit)
 
