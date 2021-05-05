67eb9de5ea467f208a97f869cbf8f0ef4c424970
KernalPanik <lukas.michnevic@protonmail.com>
2021-05-05 14:34:46 +0300

    Modified verification function

--- Utils.py
+++ Utils.py
@@ -2,9 +2,10 @@ import csv
 from qiskit import Aer, execute
 from Scheduler.DagHandler import hash_adj_list
 import qiskit
+from qiskit.visualization import plot_histogram
 
 def compare_qasm_execs(qasm_file_1, qasm_file_2) -> bool:
-    simulator = Aer.get_backend('qasm_simulator')
+    simulator = Aer.get_backend('statevector_simulator')
     qc1 = qiskit.QuantumCircuit.from_qasm_file(qasm_file_1)
     qc2 = qiskit.QuantumCircuit.from_qasm_file(qasm_file_2)
     
@@ -14,7 +15,22 @@ def compare_qasm_execs(qasm_file_1, qasm_file_2) -> bool:
     job2 = execute(qc2, simulator, shots=1000)
     result2 = job2.result()
 
-    return result1.get_counts(qc1) == result2.get_counts(qc2)
+    counts1 = list(result1.get_counts().values())
+    counts2 = list(result2.get_counts().values())
+    try:
+        fig1 = plot_histogram(result1, color='midnightblue', title="New Histogram")
+        fig2 = plot_histogram(result2, color='midnightblue', title="New Histogram")
+        
+        fig1.savefig(qasm_file_1)
+        fig2.savefig(qasm_file_2)
+    except ImportError:
+        print("Failed to locate libraries to generate histograms..")
+
+    for i in range(0, len(counts1)):
+        if(counts1[i] - counts2[i] > 0.000001):
+            return False
+    
+    return True
 
 def put_subdags_into_csv(csv_path: str, subdags: list):
     with open(csv_path, 'a', newline='') as csvfile:
--- mac_test_runner.sh
+++ mac_test_runner.sh
@@ -1,4 +1,4 @@
-for file in qasms/*
+for file in qasm_files/*
 do
 	./Optimizer_m1.sh "$file"
 done
